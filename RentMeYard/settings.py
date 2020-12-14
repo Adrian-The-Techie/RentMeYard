3cb2218f4044eb8853b4830087db63865ebefb2c
Adrian-The-Techie <adrianmuthomie@gmail.com>
2020-12-14 22:02:23 +0300

    Testing something

--- RentMeYard/settings.py
+++ RentMeYard/settings.py
@@ -30,7 +30,7 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = 'wmu47e_gya7%m&takhznoq+&c%5!)!2@vfup81l+hy&81*$722'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '192.168.100.149',
